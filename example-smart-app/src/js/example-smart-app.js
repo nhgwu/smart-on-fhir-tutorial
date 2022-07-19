62bd7ad038547ec01d3a5b04d2d6d539a261ce1e
nhgwu <109095128+nhgwu@users.noreply.github.com>
2022-07-19 16:15:33 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -78,7 +78,7 @@
           p.temp = getQuantityValueAndUnit(temp[0]);
 		
           p.alg = alg.forEach(function(obj){
-			  alert(obj.code.text);
+			  obj.code.text;
 		  });
 		//p.alg = alg[0].code.text;
 		/*p.alg = alg.forEach(function(obj){
